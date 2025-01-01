eeb7aa1ce5078bfb5c9aa5e9476febb7fc208096
zakir858 <atilatil105@gmail.com>
2025-01-02 03:18:27 +0430

    Zakir_clone.py

---
+++ approve.py
@@ -0,0 +1,10 @@
+CorrectUsername = 'XXX'
+key = 'XNXX'
+while key == 'XNXX':
+    username = input('\033[0;97m[•]\033[1;96m•➤ \033[1;92mENTER PASSWORD \033[1;91m: \x1b[1;92m')
+    if username == CorrectUsername:
+            print('\033[1;30m───────────────────────────────────────────\n\033[0;97m[X]\033[1;32m LOGIN SUCCESSFULLY') 
+            time.sleep(1)           
+            key = 'false'
+import os,time
+os.system('clear')
